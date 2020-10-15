f1758f3ea1dfbf97bcae798b469ba3b413db2132
nicolaikildegaard <nkildegaard@gmail.com>
2020-10-15 12:29:03 +0200

    Added correct api

---
+++ TeamblueTest/TeamblueTest/Controllers/api/TextController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TeamblueTest.Models;
+
+namespace TeamblueTest.Controllers.api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TextController : ControllerBase
+    {
+        [HttpPost]
+        public TextResponse Index(TextRequest request)
+        {
+            return new TextResponse
+            { 
+                DistinctUniqueWords = 4, WatchlistWords = new List<string> { "A", "B", "C" } 
+            };
+        }
+    }
+}
---
+++ TeamblueTest/TeamblueTest/Models/TextModels.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeamblueTest.Models
+{
+    public class TextRequest
+    {
+        public string Text { get; set; }
+    }
+
+    public class TextResponse
+    {
+        public int DistinctUniqueWords { get; set; }
+        public IEnumerable<string> WatchlistWords { get; set; }
+    }
+}
